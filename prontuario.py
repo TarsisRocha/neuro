f3c0c5b64259cc04823c674b9b80a1576e1cf002
TarsisRocha <153626616+TarsisRocha@users.noreply.github.com>
2025-04-16 14:19:24 -0300

    Update prontuario.py

--- prontuario.py
+++ prontuario.py
@@ -1,20 +1,15 @@
 # prontuario.py
-from banco_dados import obter_conexao
+from banco_dados import supabase
 
 def adicionar_prontuario(paciente_id, descricao, data):
-    conexao = obter_conexao()
-    cursor = conexao.cursor()
-    cursor.execute(
-        'INSERT INTO prontuarios (paciente_id, descricao, data) VALUES (?, ?, ?)',
-        (paciente_id, descricao, data)
-    )
-    conexao.commit()
-    conexao.close()
+    dados = {
+        "paciente_id": paciente_id,
+        "descricao": descricao,
+        "data": data
+    }
+    resposta = supabase.table("prontuarios").insert(dados).execute()
+    return resposta
 
 def obter_prontuarios_por_paciente(paciente_id):
-    conexao = obter_conexao()
-    cursor = conexao.cursor()
-    cursor.execute('SELECT * FROM prontuarios WHERE paciente_id = ?', (paciente_id,))
-    prontuarios = cursor.fetchall()
-    conexao.close()
-    return prontuarios
+    resposta = supabase.table("prontuarios").select("*").eq("paciente_id", paciente_id).execute()
+    return resposta.data
